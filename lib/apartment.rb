fd8b5b59605811f668f8c962d28328d4103544ba
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-11 09:25:41 -0700

    Initialize apartment object with attributes, pass initial tests

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,1 +1,20 @@
+class Apartment
 
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
+  def initialize(info)
+    @info = info
+    @number = info[:number]
+    @monthly_rent = info[:monthly_rent]
+    @bathrooms = info[:bathrooms]
+    @bedrooms = info[:bedrooms]
+    @renter = info[:renter]
+  end
+
+  
+
+end
