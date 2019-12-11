201a8ecff78fe12c2673ffeda20d50e40798f8e4
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-11 10:21:59 -0700

    Update building class to include setup

--- test/building_test.rb
+++ test/building_test.rb
@@ -5,38 +5,35 @@ require './lib/renter'
 require './lib/apartment'
 
 class BuildingTest < Minitest::Test
-  def test_it_exists
-    building = Building.new
 
-    assert_instance_of Building, building
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 3, bedrooms: 2})
+    @renter1 = Renter.new("Spencer")
+    
+    assert_instance_of Building, @building
   end
 
   def test_it_starts_without_units
-    building = Building.new
 
-    assert_equal [], building.units
+    assert_equal [], @building.units
   end
 
   def test_it_can_add_units
-    building = Building.new
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
 
-    building.add_unit(unit1)
-    building.add_unit(unit2)
-
-    assert_equal [unit1, unit2], building.units
+    assert_equal [@unit1, @unit2], @building.units
   end
 
   def test_it_has_average_rent
-    building = Building.new
-    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
 
-    building.add_unit(unit1)
-    building.add_unit(unit2)
-
-    assert_equal 1099.5, building.average_rent
+    assert_equal 1099.5, @building.average_rent
   end
 
 end
