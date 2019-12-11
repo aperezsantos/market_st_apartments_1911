f8ed9e0b8f518148c32f356a887dcb4a493ca86b
Ana Perez Santos <a.jperez@yahoo.com>
2019-12-11 10:17:19 -0700

    Test/Method for calculating average rent, pass tests

--- lib/building.rb
+++ lib/building.rb
@@ -11,11 +11,12 @@ class Building
 
   def average_rent
     # look at each unit's rent (iterate)
-    @units.map do |unit|
-      unit.monthly_rent
+    rents = @units.map do |unit|
+      unit.monthly_rent.to_f
     end
-    # add them together
-    # devide them by the number of objects
-
+    # require "pry"; binding.pry
+    # add them together, and divide by the number of units
+    (rents.sum) / @units.count
   end
+
 end
